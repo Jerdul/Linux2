fdb7aa953b4c0ac570c9009abc5408c04037a76c
Ludvig <ludvig.jerner@yh.nackademin.se>
2021-09-14 22:37:26 +0200

    some last touches...

--- monitor_cronjob.sh
+++ monitor_cronjob.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
-# IF NOT EXIST mkdir "monitoring_logs"
-mkdir monitoring_logs
-cd monitoring_logs
 
 # Time stamp current time
 current_time=$(date "+%Y-%m.%d-%H.%M.%S")
 
-# create cronjob 
-# run monitoring script
-. monitoring_script.sh > "monitor_$current_time.log"
 
-# check that file-name (date-stamp) doesn't exist
 
-# compress file and save
+# IF NOT EXIST mkdir "monitoring_logs"
+dir_name="monitor_logs"
+if [[ ! -d "$dir_name" ]]
+then
+    mkdir $dir_name
+fi         
+
+
+
+# TODO: create cronjob 
+# run monitoring script, compress and save to log file
+. monitor_script.sh | gzip > /$dir_name/"monitor_$current_time.log"
+
 
 
--- monitoring_script.sh
+++ monitoring_script.sh
@@ -1,42 +1,48 @@
 #!/bin/bash
 
-# creating log file
-
-# TODO: 
-# if not exists OR increment name with versio-nr/date-stamp
-touch monitor_log.log
 
 # format for time stamp in file-names
 current_time=$(date "+%Y.%m.%d-%H.%M.%S")
 
+to_file="monitor_$(current_time).log
+"
 # printing timestamp to log file
-date
+date > $to_file
 
 
-# --- APACHE ---
+
+
+####################
+#      APACHE      #
+####################
+
 # checking if service is active
 STATUS="$(systemctl is-active apache2)"
 if [ "${STATUS}" = "active" ]; then
-    printf  "\nApache is: ${STATUS} and running...\n"
+    printf  "\nApache is: ${STATUS} and running...\n" >> $to_file
 else
-    printf "\nApache is: ${STATUS} and not running...\n"
+    printf "\nApache is: ${STATUS} and not running...\n" >> $to_file
 fi
 
 
-# --- DATABASE MYSQL ---
+
+
+############################
+#      DATABASE MYSQL      #
+############################
+
 # checking if service active
 STATUS="$(systemctl is-active mysql)"
 if [ "${STATUS}" = "active" ]; then
-    printf "\nMysql is: ${STATUS} and running...\n"
+    printf "\nMysql is: ${STATUS} and running...\n" >> $to_file
 else
-    printf "\nMysql is: ${STATUS} and not running...\n"
+    printf "\nMysql is: ${STATUS} and not running...\n" >> $to_file
 fi
 
 # making backup of database
-mysqldump -u "<username>" -p "<password>" "<db_name>" > db_backup_"$current_time".sql
-
-# TODO:
-# if not exist -> mkdir db_backups
+# OLD COMMAND af LUDVIG: mysqldump -u "<username>" -p "<password>" "<db_name>" > db_backup_"$current_time".sql
+mysql --user root --password [db_name] > [db_name].sql
+# Would it be out of place to compress this backup file??
 
 # check for backups older than 5 days
 old_backups=$(find <location> -name "*.sql" -type f -mtime +5)
@@ -45,38 +51,52 @@ old_backups=$(find <location> -name "*.sql" -type f -mtime +5)
 $old_backup -delete
 
 
-# --- AUTHENTICATION ---
-printf "\nAuthentication Failures: \n"
-# printing usernames to log file
-grep "authentication failure" /var/log/auth.log | cut -d '=' -f 8
 
-printf "\nAccepted logins: \n"
-grep "accepted password" /var/log/auth.log | cut -d '=' -f 8
+
+############################
+#      AUTHENTICATION      #
+############################
+
+printf "\nAuthentication Failures: \n" >> $to_file
+# printing usernames to log file
+grep "authentication failure" /var/log/auth.log | cut -d '=' -f 8 >> $to_file
+
+printf "\nAccepted logins: \n" >> $to_file
+grep "accepted password" /var/log/auth.log | cut -d '=' -f 8 >> $to_file
 
 # --- FILE-PERMISSIONS ---
-find /var/log -printf "\nFile name: %f | Groups: %m | ID: %i | Permissions: (%M) \n_____________________________________________________________\n"
+find /var/log -printf "\nFile name: %f | Groups: %m | ID: %i | Permissions: (%M) \n_____________________________________________________________\n" >> $to_file
 
 
-# --- PORTS ---
+
+
+###################
+#      PORTS      #
+###################
+
 # check listening ports
-sudo lsof -i -P -n | grep LISTEN
+lsof -i -P -n | grep LISTEN >> $to_file
 
-# check specific port. Example port 22:
-sudo lsof -i:22
+# IF WANTED: check specific port. Example port 22:
+lsof -i:22 >> $to_file
 
 
-# --- STORAGE ---
+
+
+#####################
+#      STORAGE      #
+#####################
 
 df -H | grep -vE '^Filesystem|tmpfs|cdrom' | awk '{ print $5 " " $1 }' | while read output;
 do
-  echo $output
+  echo $output >> $to_file
   if [[ $output == *"loop"* ]]; then
-    printf ""
+    printf "" >> $to_file
   else
     usep=$(echo $output | awk '{ print $1}' | cut -d'%' -f1  )
     partition=$(echo $output | awk '{ print $2 }' )
     if [ $usep -ge 80 ]; then
-      echo "Running out of space \"$partition ($usep%)\" on $(hostname) as on $(date)" # |
+      echo "Running out of space \"$partition ($usep%)\" on $(hostname) as on $(date)" # | >> $to_file
       # mail -s "Alert: Almost out of disk space $usep%" admin@server.se
     fi
   fi
